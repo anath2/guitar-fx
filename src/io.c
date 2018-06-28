20edcec72198ca234e1b7beb80f3638cf4dbac62
Ajit Nath <nath.ajit01@gmail.com>
2018-06-28 20:50:45 +0800

    cleanup

---
+++ src/io.c
@@ -0,0 +1,26 @@
+/*
+  Read sound data from line-in and write to line-out
+ */
+
+#include <stdio.h>
+# include "portaudio.h"
+
+
+#define SAMPLE_RATE    (44100)
+#define NUM_CHANNELS   (2)
+
+
+static int recordCallBack() {
+  /*
+   * Record audio
+  */
+  return 0;
+}
+
+
+static int playCallBack() {
+  /*
+   * Play audio
+   */
+  return 0;
+}
